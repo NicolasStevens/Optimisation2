42fa83fc815a8fd4a5eaa49be331f363030e3400
Nicolas Stevens <nicolas.20.07@hotmail.com>
2014-12-20 00:41:43 +0100

    model update

--- Devoir3/Code/constraintForm.m
+++ Devoir3/Code/constraintForm.m
@@ -1,48 +1,105 @@
-function [A,b,c] = constraintForm(Node,Triangle,n,m,k)
+function [A,b,c] = constraintForm(N)
 %
-% m is the number of nodes
+% N
+%
+
+
 % n is the number of triangles
 % k is the number of nodes on each border. 
-% Node contains the (x,y) coordinate of the nodes. The nodes are sorted
-% such as the 4*k-4 first are the border ones, clockwise, begining at the
-% top.
-% Triangle is an array containing the triangles 
-%
-%
+% m is the number of nodes
+n = N*N*4;
+k = N+1;
 
-% 3m (and not 3*3*n!!, as we impose continuity) variable such that 
-% x=[sxx sxy syy]';
+nBNode = N*N*4*3;
 
-A=zeros(??,3*m);
+
+A=zeros(??,nbNode);
+b=zeros(??,1);
+c=zeros(3*m,1);
+
+%objective
 
 % divergence nulle :
-for i=1:n
-    % eq 1
-   nodem=Triangle(i,1);
-   noden=Triangle(i,2);
-   nodep=Triangle(i,3);
-   A(i,noden)=1/(Node(noden,1)-Node(nodem,1));
-   A(i,nodem)=-1/(Node(noden,1)-Node(nodem,1));
-   A(i,nodep)=1/(Node(nodep,1)-Node(nodem,1));
-   A(i,nodem)=-1/(Node(nodep,1)-Node(nodem,1));
-   A(i,m+noden)=1/(Node(noden,2)-Node(nodem,2));
-   A(i,m+nodem)=-1/(Node(noden,2)-Node(nodem,2));
-   A(i,m+nodep)=1/(Node(nodep,2)-Node(nodem,2));
-   A(i,m+nodem)=-1/(Node(nodep,2)-Node(nodem,2));
-   
-   % eq 2
-   A(n+i,m+noden)=1/(Node(noden,1)-Node(nodem,1));
-   A(n+i,m+nodem)=-1/(Node(noden,1)-Node(nodem,1));
-   A(n+i,m+nodep)=1/(Node(nodep,1)-Node(nodem,1));
-   A(n+i,m+nodem)=-1/(Node(nodep,1)-Node(nodem,1));
-   A(n+i,2*m+noden)=1/(Node(noden,2)-Node(nodem,2));
-   A(n+i,2*m+nodem)=-1/(Node(noden,2)-Node(nodem,2));
-   A(n+i,2*m+nodep)=1/(Node(nodep,2)-Node(nodem,2));
-   A(n+i,2*m+nodem)=-1/(Node(nodep,2)-Node(nodem,2));
+for i=1:N*N
+    indl = (i-1)*8; %indice sur les lignes (2 contraintes par triangle et 4 triangles par carre)
+    
+    %triangle type 1
+    indp1 = (i-1)*12;%indice sur les points(12 points par triangles)
+    
+    A(indl+1,indp1+1)=-1/(2*h);
+    A(indl+1,indp1+2)=-1/(2*h);
+    A(indl+1,indp1+3)=1/h;
+    A(indl+1,indp1+1+nBNode)=1/l;
+    A(indl+1,indp1+2+nBNode)=-1/l;
+    
+    A(indl+2,indp1+1+nBNode)=-1/(2*h);
+    A(indl+2,indp1+2+nBNode)=-1/(2*h);
+    A(indl+2,indp1+3+nBNode)=1/h;
+    A(indl+2,indp1+1+2*nBNode)=1/l;
+    A(indl+2,indp1+1+2*nBNode)=1/l;
+    
+    %triangle type 2
+    indp2 = (i-1)*12 + 3;%indice sur les points(12 points par triangles)
+    
+    A(indl+3,indp2+2)=-1/l;
+    A(indl+3,indp2+3)=1/l;
+    A(indl+3,indp2+1+nBNode)=1/h;
+    A(indl+3,indp2+2+nBNode)=-1/(2*h);
+    A(indl+3,indp2+3+nBNode)=-1/(2*h);
+    
+    A(indl+4,indp2+2+nBNode)=-1/l;
+    A(indl+4,indp2+3+nBNode)=1/l;
+    A(indl+4,indp2+1+2*nBNode)=1/h;
+    A(indl+4,indp2+2+2*nBNode)=-1/(2*h);
+    A(indl+4,indp2+3+2*nBNode)=-1/(2*h);
+    
+    %triangle type 3
+    indp3 = (i-1)*12 + 6;%indice sur les points(12 points par triangles)
+    
+    A(indl+5,indp3+1)=-1/h;
+    A(indl+5,indp3+2)=1/(2*h);
+    A(indl+5,indp3+3)=1/(2*h);
+    A(indl+5,indp3+2+nBNode)=-1/l;
+    A(indl+5,indp3+3+nBNode)=1/l;
+    
+    A(indl+6,indp3+1+nBNode)=-1/h;
+    A(indl+6,indp3+2+nBNode)=1/(2*h);
+    A(indl+6,indp3+3+nBNode)=1/(2*h);
+    A(indl+6,indp3+2+2*nBNode)=-1/l;
+    A(indl+6,indp3+3+2*nBNode)=1/l;
+    
+    %triangle type 4
+    indp4 = (i-1)*12 + 3;%indice sur les points(12 points par triangles)
+    
+    A(indl+7,indp4+1)=-1/l;
+    A(indl+7,indp4+3)=1/l;
+    A(indl+7,indp4+1+nBNode)=1/(2*h);
+    A(indl+7,indp4+2+nBNode)=-1/h;
+    A(indl+7,indp4+3+nBNode)=1/(2*h);
+    
+    A(indl+8,indp4+1+nBNode)=-1/l;
+    A(indl+8,indp4+3+nBNode)=1/l;
+    A(indl+8,indp4+1+2*nBNode)=1/(2*h);
+    A(indl+8,indp4+2+2*nBNode)=-1/h;
+    A(indl+8,indp4+3+2*nBNode)=1/(2*h);
+    
+    %continuite
+    div = 8*N*N;
+    indcontL = (i-1)*8;
+    A(indl+8,indp4+3+2*nBNode)=1/(2*h);
+    
+    
 end
 
-% boundary conditions
 
+
+
+
+
+
+% boundary conditions
+A(2*n+1:2*n+k,1:k)         = eye(k);  
+A(2*n+k+1:2*n+2*k,m+1:m+k) = eye(k);
 
 
 
